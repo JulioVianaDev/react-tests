4d4b2ce848a517344c274407b31d60b03baaa898
juliovianadev <juliovianadev@gmail.com>
2023-04-07 18:22:11 -0300

    testes funcionando

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Greeting() {
+  const [changeText,setChangeText] = useState(false)
+
+  const change=(e)=>{
+    setChangeText(true);
+  }
   return (
     <div>
       <h2>Batata</h2>
       <p>Uia</p>
+      {changeText && <p>texto alterado</p>}
+      {!changeText && <p>texto normal</p>}<br></br>
+      <button onClick={change}>Click</button>
     </div>
   )
 }
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,10 +1,32 @@
 import { render ,screen} from "@testing-library/react"
 import Greeting from "./Greeting"
+import userEvent from "@testing-library/user-event"
+
 describe('Greeting component',()=>{
   test('testando o greeting',()=>{
     render(<Greeting/>)
 
     const  batataElement = screen.getByText("Batata")
     expect(batataElement).toBeInTheDocument()
+  })
+  test('testando o useState',()=>{
+    render(<Greeting/>)
+
+    const  paragraphElement = screen.getByText("texto normal",{exact: false});
+    expect(paragraphElement).toBeInTheDocument();
+  })
+  test('verificando se altera quando o botão é clicado',()=>{
+    render(<Greeting/>);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    const outputElement = screen.getByText('texto alterado',{exact: false});
+    expect(outputElement).toBeInTheDocument()
+  })
+  test('checar se não existe na tela o conteudo texto normal',()=>{
+    render(<Greeting/>);
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+    const outputElement = screen.queryByText('texto normal',{exact: false});
+    expect(outputElement).toBeNull()
   })
 })
