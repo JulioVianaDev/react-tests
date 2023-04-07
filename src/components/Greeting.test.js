1b4d0dd97d8c694d38bfc04e8ac6eefa82459da9
juliovianadev <juliovianadev@gmail.com>
2023-04-07 17:57:00 -0300

    group tests

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,9 +1,10 @@
 import { render ,screen} from "@testing-library/react"
 import Greeting from "./Greeting"
-test('testando o greeting',()=>{
-  render(<Greeting/>)
+describe('Greeting component',()=>{
+  test('testando o greeting',()=>{
+    render(<Greeting/>)
 
-  const  batataElement = screen.getByText("Batata")
-  expect(batataElement).toBeInTheDocument()
-
+    const  batataElement = screen.getByText("Batata")
+    expect(batataElement).toBeInTheDocument()
+  })
 })
