e6908d02def6756bc66ec4f5bd317d5ec203f38b
juliovianadev <juliovianadev@gmail.com>
2023-04-08 01:13:09 -0300

    test mockado

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,10 @@ import Async from "./Async"
 
 describe('async component',()=>{
   test('render post if success',async ()=>{
+    window.fetch = jest.fn()
+    window.fetch.mockResolvedValueOnce({
+      json: async()=>[{id: 1,title: 'um'}]
+    })
     render(<Async/>)
 
     const listElements= await screen.findAllByRole('listitem');
